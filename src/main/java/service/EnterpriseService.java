ea4908a7f4b457f0c16534d0d413d4ed4887b1d6
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-03-18 14:26:03 -0300

    update enterprise

--- src/main/java/service/EnterpriseService.java
+++ src/main/java/service/EnterpriseService.java
@@ -1,21 +1,34 @@
 package service;
 
 
-import dto.EnterpriseDTO;
+import entity.Enterprise;
 import lombok.AllArgsConstructor;
-import mapper.EnterpriseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.EnterpriseRepository;
+import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class EnterpriseService {
-    private final EnterpriseRepository enterpriseRepository;
-    private final EnterpriseMapper enterpriseMapper;
 
-    Optional<EnterpriseDTO> buscarUUID(UUID uuid) {
-        return enterpriseRepository.findByUuid(uuid).map(enterpriseMapper::toDto);
+    @Autowired
+    private EnterpriseRepository enterpriseRepository;
+
+    public List<Enterprise> getAllEnterprises() {
+        return enterpriseRepository.findAll();
+    }
+
+    public Optional<Enterprise> getEnterpriseById(Long id) {
+        return enterpriseRepository.findById(id);
+    }
+
+    public Enterprise saveEnterprise(Enterprise enterprise) {
+        return (Enterprise) enterpriseRepository.save(enterprise);
+    }
+
+    public void deleteEnterprise(Long id) {
+        enterpriseRepository.deleteById(id);
     }
 }
