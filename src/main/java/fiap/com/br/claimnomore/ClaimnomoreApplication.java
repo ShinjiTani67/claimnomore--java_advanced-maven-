6466136a6c6f5083ad8578b8f2c460a063405d2b
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-03-18 09:20:03 -0300

    add projeto

---
+++ src/main/java/fiap/com/br/claimnomore/ClaimnomoreApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.claimnomore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimnomoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimnomoreApplication.class, args);
+	}
+
+}
