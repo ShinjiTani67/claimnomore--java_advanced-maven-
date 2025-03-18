d041368e41d0ce0cd62ab70e40e031bdc3d3479e
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-03-18 09:28:49 -0300

    create dentist

---
+++ src/main/java/entity/Dentist.java
@@ -0,0 +1,38 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Data
+@ToString
+@Entity
+@Table(name="tb_dentist")
+public class Dentist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name_dentist", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "cpf_dentist", length = 100, nullable = false)
+    private int CPF;
+
+    @Column(name = "cro_dentist", length = 100, nullable = false)
+    private String CRO;
+
+    @Column(name = "adress_dentist", length = 100, nullable = false)
+    private String adress;
+
+    @Column(name = "phone_dentist", length = 100, nullable = false)
+    private int phone;
+
+    @Column(name = "email_dentist", length = 100, nullable = false)
+    private String email;
+
+    @DateTimeFormat
+    private int birthdate;
+}
