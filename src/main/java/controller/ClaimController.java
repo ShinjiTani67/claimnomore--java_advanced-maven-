9ec0538e892a582d4781cc54a321a738f065ff95
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-03-18 14:16:34 -0300

    update claimcontroller

--- src/main/java/controller/ClaimController.java
+++ src/main/java/controller/ClaimController.java
@@ -1,14 +1,14 @@
 package controller;
 
-import dto.ClaimDTO;
+import entity.Claim;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import service.ClaimService;
 
-import java.util.UUID;
+import java.util.List;
+
 
 
 @RestController
@@ -17,17 +17,38 @@ import java.util.UUID;
 public class ClaimController {
 
     @Autowired
-    private final ClaimService claimService;
+    private ClaimService claimService;
 
-    @PutMapping("/{uuid}")
-    public ResponseEntity<?> atualizarClaim(@RequestBody ClaimDTO claimDto,
-                                             @PathVariable UUID uuid) {
-        try {
-            claimDto.setUuid(uuid);
-            return new ResponseEntity<>(claimService.atualizarClaim(claimDto), HttpStatus.OK);
-        } catch (IllegalArgumentException e) {
+    @GetMapping("/list")
+    public String listClaims(Model model) {
+        List<Claim> claims = claimService.getAllClaims();
+        model.addAttribute("claims", claims);
+        return "claim-list"; // Nome da página Thymeleaf
+    }
 
-            return new ResponseEntity<>("Tarefa com UUID não existe.", HttpStatus.NOT_FOUND);
-        }
+    @GetMapping("/form")
+    public String showClaimForm(Model model) {
+        model.addAttribute("claim", new Claim());
+        return "claim-form";
+    }
+
+    @PostMapping("/save")
+    public String saveClaim(@ModelAttribute Claim claim) {
+        claimService.saveClaim(claim);
+        return "redirect:/claim/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editClaim(@PathVariable Long id, Model model) {
+        Claim claim = claimService.getClaimById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Reclamação inválida: " + id));
+        model.addAttribute("claim", claim);
+        return "claim-form";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteClaim(@PathVariable Long id) {
+        claimService.deleteClaim(id);
+        return "redirect:/claim/list";
     }
 }
