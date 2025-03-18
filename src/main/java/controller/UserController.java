18b0138ebde161094ee485ddadb618b0d0f8686b
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-03-18 14:10:33 -0300

    update user

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,8 +2,10 @@ package controller;
 
 import entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import repository.UserRepository;
+import service.UserService;
 
 import java.util.List;
 
@@ -14,15 +16,37 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
-    @GetMapping
-    public List<User> getAllUser(){
-        return userRepository.findAll();
-    }
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return (User) userRepository.save(user);
+    @GetMapping("/list")
+    public String listUsers(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "user-list"; // Nome do arquivo HTML
     }
 
+    @GetMapping("/form")
+    public String showUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "user-form";
+    }
+
+    @PostMapping("/save")
+    public String saveUser(@ModelAttribute User user) {
+        userService.saveUser(user);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editUser(@PathVariable Long id, Model model) {
+        User user = userService.getUserById(id).orElseThrow(() -> new IllegalArgumentException("Usuário inválido: " + id));
+        model.addAttribute("user", user);
+        return "user-form";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return "redirect:/user/list";
+    }
 }
