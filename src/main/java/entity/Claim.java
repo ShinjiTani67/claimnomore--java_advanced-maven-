463136b9ed04852707b0773dd10598e117c6af14
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-03-18 09:27:24 -0300

    create claim.java

---
+++ src/main/java/entity/Claim.java
@@ -0,0 +1,23 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.ZonedDateTime;
+@Data
+@ToString
+@Entity
+@Table(name = "tb_claim")
+public class Claim {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "claim_date", length = 100, nullable = false)
+    private ZonedDateTime claimDate;
+
+    @Column(name = "claim_description", length = 100, nullable = false)
+    private String description;
+}
