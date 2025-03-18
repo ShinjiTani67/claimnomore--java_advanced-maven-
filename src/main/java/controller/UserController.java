2ebe3a168e0fdf2bd0de313812fc16852a8cd2c2
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-03-18 09:44:05 -0300

    update geral

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,6 +1,6 @@
 package controller;
 
-import model.User;
+import entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import repository.UserRepository;
