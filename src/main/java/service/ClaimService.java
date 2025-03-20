54fd7ad998f77d818b1506c84a91a1a95943fc6e
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-03-20 18:41:48 -0300

    update claim service

--- src/main/java/service/ClaimService.java
+++ src/main/java/service/ClaimService.java
@@ -1,21 +1,17 @@
 package service;
 
-import dto.ClaimDTO;
 import entity.Claim;
 import lombok.AllArgsConstructor;
-import mapper.ClaimMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import repository.ClaimRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @AllArgsConstructor
-public class ClaimService   {
+public class ClaimService {
 
     @Autowired
     private ClaimRepository claimRepository;
@@ -30,8 +26,9 @@ public class ClaimService   {
 
     public Claim saveClaim(Claim claim) {
         return claimRepository.save(claim);
+    }
 
-        public void deleteClaim(Long id) {
+    public void deleteClaim(Long id) {
         claimRepository.deleteById(id);
     }
 }
